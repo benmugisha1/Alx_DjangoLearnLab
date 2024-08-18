8fbdc8f1cae67c6437466ca240de03b13f5bfd8b
benmigisha1 <mugishabenjohnson@gmail.com>
2024-08-18 21:03:00 +0200

    Saving 7

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,3 +5,17 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+
+# relationship_app/urls.py
+
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, register
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    # Add other URL patterns as needed
+]
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -20,3 +20,28 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# relationship_app/views.py
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the appropriate view name
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
