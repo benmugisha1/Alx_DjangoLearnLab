72626fd1e8e965352d96697f9c011468846dee04
benmigisha1 <mugishabenjohnson@gmail.com>
2024-08-18 19:52:16 +0200

    Saving 5

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,3 +5,11 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()  # Query to get all book instances
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+from django.shortcuts import render, get_object_or_404
+from .models import Library
+
+# View to display library details
+def library_detail(request, pk):
+    library = get_object_or_404(Library, pk=pk)  # Query to get a specific library by primary key
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
