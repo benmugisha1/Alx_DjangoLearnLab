125a08de2350f362e8de89338026bd8677ee8421
benmigisha1 <mugishabenjohnson@gmail.com>
2024-08-18 19:50:11 +0200

    Saving 4

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Ensure this line is present
-from .models import Author, Book, Library, Librarian
+from .models import Book
 
-# Example DetailView for a Book
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'relationship_app/book_detail.html'
-
-# Example DetailView for an Author
-class AuthorDetailView(DetailView):
-    model = Author
-    template_name = 'relationship_app/author_detail.html'
-
-# Other views can be added below
+# View to list all books
+def list_books(request):
+    books = Book.objects.all()  # Query to get all book instances
+    return render(request, 'relationship_app/list_books.html', {'books': books})
