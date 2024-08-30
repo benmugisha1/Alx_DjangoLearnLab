e3ed6abf566097680459c8e718bd0b9141ef074e
benmigisha1 <mugishabenjohnson@gmail.com>
2024-08-30 16:44:56 +0200

    First commitssd

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -25,3 +25,11 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    ...
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -19,3 +19,12 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
