099653d35f35e6ad95a891647c50f7c6cac025db
benmigisha1 <mugishabenjohnson@gmail.com>
2024-09-06 15:32:01 +0200

    Commit 6

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -31,3 +31,36 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]  # Only authenticated users can delete
+
+
+from .filters import BookFilter
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filterset_class = BookFilter
+
+
+    from rest_framework.filters import SearchFilter
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter)
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+
+
+# views.py
+from rest_framework.filters import OrderingFilter
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default ordering
+
