edbb2a0e21945aca461d64aaa885397f8d82a762
benmigisha1 <mugishabenjohnson@gmail.com>
2024-09-06 15:38:05 +0200

    Commit 7

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,9 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-
+from .filters import BookFilter
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
 
 # List and Detail views: Readable by anyone
@@ -10,6 +12,11 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]  # Anyone can read
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default ordering
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -33,34 +40,11 @@ class BookDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]  # Only authenticated users can delete
 
 
-from .filters import BookFilter
-
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
-    filterset_class = BookFilter
 
 
-    from rest_framework.filters import SearchFilter
-
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter)
-    filterset_class = BookFilter
-    search_fields = ['title', 'author']
 
 
-# views.py
-from rest_framework.filters import OrderingFilter
 
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter)
-    filterset_class = BookFilter
-    search_fields = ['title', 'author']
-    ordering_fields = ['title', 'publication_year']
-    ordering = ['title']  # Default ordering
+ 
+
 
