b643493270279e7690a0437ffe33a77969c8d5f5
benmigisha1 <mugishabenjohnson@gmail.com>
2024-09-15 15:56:05 +0200

    First commits

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+
+from django.contrib.auth.forms import UserChangeForm
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'blog/edit_profile.html', {'form': form})
+
