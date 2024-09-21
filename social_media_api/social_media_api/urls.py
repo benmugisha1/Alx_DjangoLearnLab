5d0b95156b77ed38e983db5459eef92bdb5300d6
benmigisha1 <mugishabenjohnson@gmail.com>
2024-09-21 20:08:03 +0200

    First commit

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('accounts.urls')),  # For user authentication
+    path('api/', include('posts.urls')),     # Include the posts app's URLs
+]
