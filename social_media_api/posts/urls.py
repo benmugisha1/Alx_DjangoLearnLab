1ef0aafd6b56c132a1d764aade3d938109684579
benmigisha1 <mugishabenjohnson@gmail.com>
2024-09-21 20:04:32 +0200

    First commit

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from . import views
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'comments', views.CommentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
